dd499697f41e3e2ba34a28b9f4ef691a1444a0bb
marineLM <marine-le-morvan@hotmail.fr>
2023-08-22 09:33:06 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
 import setuptools
-# from setuptools import setup
-
-# setup(
-#     name='neumiss',
-#     version='0.0.1',
-#     author='Marine Le Morvan and Alexandre Perez-Lebel',
-#     author_email='alexandre.perez@inria.fr',
-#     packages=['neumiss', 'neumiss.test'],
-#     scripts=['bin/script1', 'bin/script2'],
-#     url='http://pypi.python.org/pypi/PackageName/',
-#     license='LICENSE.txt',
-#     description='An awesome package that does something',
-#     long_description=open('README.txt').read(),
-#     install_requires=[
-#         "Django >= 1.1.1",
-#         "pytest",
-#     ],
-# )
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
